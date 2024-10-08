afeadd7820b30b715671499a48f2798f52caea8d
Shazi-dani <shazinamunir26@gmail.com>
2024-10-08 14:07:38 +0500

    finished product color logic

--- bag/views.py
+++ bag/views.py
@@ -1,21 +1,35 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
-# Create your views here.
 
 def view_bag(request):
     """ A view that renders the bag contents page """
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
     quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+    redirect_url = request.META.get('HTTP_REFERER', '/')
+    color = None
+
+    if 'product_color' in request.POST:
+        color = request.POST['product_color']
+
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if color:
+        if item_id in list(bag.keys()):
+            if color in bag[item_id]['items_by_color'].keys():
+                bag[item_id]['items_by_color'][color] += quantity
+            else:
+                bag[item_id]['items_by_color'][color] = quantity
+        else:
+            bag[item_id] = {'items_by_color': {color: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
