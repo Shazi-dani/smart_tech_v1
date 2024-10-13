492f84e0292e4c40f62177ba25d267af62e87a3d
Shazi-dani <shazinamunir26@gmail.com>
2024-10-13 22:34:28 +0500

    WIP: having issue in receiving client secret

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,7 @@ from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
 from bag.contexts import bag_contents
+from pprint import pprint
 
 import stripe
 import json
@@ -30,6 +31,8 @@ def cache_checkout_data(request):
         return HttpResponse(content=e, status=400)
 
 def checkout(request):
+    print("Full Request Object:")
+    pprint(vars(request), indent=2)
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -50,7 +53,14 @@ def checkout(request):
 
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+
+            pid = request.POST.get('client_secret').split('_secret')[0]
+
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+
+            order.save()
 
             for item_id, item_data in bag.items():
                 try:
