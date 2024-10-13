0006ba3ed06c5c21557288ee7add13f0aa5430b4
Shazi-dani <shazinamunir26@gmail.com>
2024-10-13 21:08:01 +0500

    added checkout form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
+from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
-from .forms import OrderForm
 from bag.contexts import bag_contents
 
 import stripe
+import json
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(
+            pid,
+            metadata={
+                'bag': json.dumps(request.session.get('bag', {})),
+                'save_info': request.POST.get('save_info'),
+                'username': request.user,
+            }
+        )
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -35,7 +55,6 @@ def checkout(request):
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
-
                     if isinstance(item_data, int):
                         order_line_item = OrderLineItem(
                             order=order,
@@ -52,7 +71,6 @@ def checkout(request):
                                 product_color=color,
                             )
                             order_line_item.save()
-
                 except Product.DoesNotExist:
                     messages.error(request, (
                         "One of the products in your bag wasn't found in our database. "
@@ -64,12 +82,10 @@ def checkout(request):
             # Check if the user wants to save their information
             if 'save_info' in request.POST:
                 request.session['save_info'] = 'save-info'
-                
+
             return redirect(reverse('checkout_success', args=[order.order_number]))
-        
         else:
             messages.error(request, 'There was an error with your form. Please double check your information.')
-
     else:
         bag = request.session.get('bag', {})
         if not bag:
@@ -100,8 +116,6 @@ def checkout(request):
         }
 
         return render(request, template, context)
-
-
 
 def checkout_success(request, order_number):
     """
