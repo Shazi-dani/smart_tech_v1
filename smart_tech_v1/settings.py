4c7bd179df2598f1a04046739f352df25a8e1434
Shazi-dani <shazinamunir26@gmail.com>
2024-10-17 19:12:33 +0500

    added cache control

--- smart_tech_v1/settings.py
+++ smart_tech_v1/settings.py
@@ -168,6 +168,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Heroku
     AWS_STORAGE_BUCKET_NAME = 'smart-tech-v1'
     AWS_S3_REGION_NAME = 'eu-north-1'
