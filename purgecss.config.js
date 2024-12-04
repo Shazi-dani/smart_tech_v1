46c7f8279a8796daa4bd0d63c5702b31b0d7062b
Shazi-dani <shazinamunir26@gmail.com>
2024-12-04 11:02:52 +0500

    configured clean css utility

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,13 +1,12 @@
 module.exports = {
     content: [
-      './**/templates/**/*.html', // Include all templates across all apps
-      './**/static/**/*.js',      // Include all JavaScript files across all apps
-      './**/*.py',                // Include Python files to catch inline CSS in strings
+      './**/templates/**/*.html',
+      './**/static/**/*.js',    
+      './**/*.py',                
     ],
-    css: ['./**/static/**/*.css'], // Include all CSS files in the static folders
-    output: './cleaned-css/',      // Output cleaned CSS to a dedicated directory
-    safelist: {                    // Optional: Define classes or selectors to exclude from purging
+    css: ['./**/static/**/*.css'], 
+    output: './cleaned-css/',     
+    safelist: {                  
       standard: ['active', 'open', 'nav-item'], 
     },
   };
-  
