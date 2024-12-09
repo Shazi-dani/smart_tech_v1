57de84aa91701f2f780ce28c9fa75161d8efac87
Shazi-dani <shazinamunir26@gmail.com>
2024-12-09 09:08:00 +0500

    updated settings

--- smart_tech_app/settings.py
+++ smart_tech_app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['smart-tech-42a1ef4a43e4.herokuapp.com', '127.0.0.1']
 
